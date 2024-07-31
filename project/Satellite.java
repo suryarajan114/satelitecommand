5cca9cdb01068725a9422baa6a0ecc5bf7241b08
suryarajan114 <surya19062004@gmail.com>
2024-07-31 15:48:39 +0530

    Update Satellite.java

--- project/Satellite.java
+++ project/Satellite.java
@@ -3,39 +3,45 @@ public class Satellite {
     private boolean solarPanelsActive;
     private int dataCollected;
 
-    // Initial state
     public Satellite() {
         this.orientation = "North";
         this.solarPanelsActive = false;
         this.dataCollected = 0;
     }
 
-    // Getters and Setters
-    public String getOrientation() {
-        return orientation;
-    }
-
     public void setOrientation(String orientation) {
         this.orientation = orientation;
     }
 
-    public boolean areSolarPanelsActive() {
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void activatePanels() {
+        this.solarPanelsActive = true;
+    }
+
+    public void deactivatePanels() {
+        this.solarPanelsActive = false;
+    }
+
+    public boolean arePanelsActive() {
         return solarPanelsActive;
     }
 
-    public void setSolarPanelsActive(boolean solarPanelsActive) {
-        this.solarPanelsActive = solarPanelsActive;
+    public void collectData() {
+        if (solarPanelsActive) {
+            this.dataCollected += 10;
+        }
     }
 
     public int getDataCollected() {
         return dataCollected;
     }
 
-    public void incrementDataCollected(int amount) {
-        if (solarPanelsActive) {
-            this.dataCollected += amount;
-        } else {
-            System.out.println("Cannot collect data. Solar panels are inactive.");
-        }
+    public void displayStatus() {
+        System.out.println("Orientation: " + orientation);
+        System.out.println("Solar Panels: " + (solarPanelsActive ? "Active" : "Inactive"));
+        System.out.println("Data Collected: " + dataCollected);
     }
 }
