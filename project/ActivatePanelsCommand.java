c1b7c540a17f505a8e02bbd69eb856ccdeabc11f
suryarajan114 <surya19062004@gmail.com>
2024-07-31 15:45:44 +0530

    Update ActivatePanelsCommand.java

--- project/ActivatePanelsCommand.java
+++ project/ActivatePanelsCommand.java
@@ -1,12 +1,5 @@
 public class ActivatePanelsCommand {
-    private Satellite satellite;
-
-    public ActivatePanelsCommand(Satellite satellite) {
-        this.satellite = satellite;
-    }
-
-    public void execute() {
-        satellite.setSolarPanelsActive(true);
-        System.out.println("Solar panels activated.");
+    public void activate(Satellite satellite) {
+        satellite.activatePanels();
     }
 }
