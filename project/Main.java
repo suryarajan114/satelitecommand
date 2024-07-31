f469d7b0d3472b1827b9a1f404d5fe77d2376958
suryarajan114 <surya19062004@gmail.com>
2024-07-31 15:44:43 +0530

    Update Main.java

--- project/Main.java
+++ project/Main.java
@@ -1,19 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Satellite satellite = new Satellite();
+        RotateCommand rotateCommand = new RotateCommand();
+        ActivatePanelsCommand activatePanelsCommand = new ActivatePanelsCommand();
+        DeactivatePanelsCommand deactivatePanelsCommand = new DeactivatePanelsCommand();
+        CollectDataCommand collectDataCommand = new CollectDataCommand();
+        Scanner scanner = new Scanner(System.in);
 
-        RotateCommand rotateCommand = new RotateCommand(satellite);
-        ActivatePanelsCommand activatePanelsCommand = new ActivatePanelsCommand(satellite);
-        CollectDataCommand collectDataCommand = new CollectDataCommand(satellite);
+        System.out.println("Satellite Command System");
+        boolean running = true;
 
-        // Example command execution
-        rotateCommand.execute("South");
-        activatePanelsCommand.execute();
-        collectDataCommand.execute();
-        
-        System.out.println("Current State:");
-        System.out.println("Orientation: " + satellite.getOrientation());
-        System.out.println("Solar Panels: " + (satellite.areSolarPanelsActive() ? "Active" : "Inactive"));
-        System.out.println("Data Collected: " + satellite.getDataCollected());
+        while (running) {
+            System.out.println("\nEnter a command (rotate, activatePanels, deactivatePanels, collectData, status, exit):");
+            String command = scanner.nextLine();
+
+            switch (command) {
+                case "rotate":
+                    System.out.println("Enter direction (North, South, East, West):");
+                    String direction = scanner.nextLine();
+                    rotateCommand.rotate(satellite, direction);
+                    break;
+                case "activatePanels":
+                    activatePanelsCommand.activate(satellite);
+                    break;
+                case "deactivatePanels":
+                    deactivatePanelsCommand.deactivate(satellite);
+                    break;
+                case "collectData":
+                    collectDataCommand.collectData(satellite);
+                    break;
+                case "status":
+                    satellite.displayStatus();
+                    break;
+                case "exit":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Unknown command. Please try again.");
+            }
+        }
+
+        scanner.close();
     }
 }
