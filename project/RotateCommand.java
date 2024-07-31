59d467679bd5d64d50e3a8d51d6a9b9e90fd7584
suryarajan114 <surya19062004@gmail.com>
2024-07-31 15:47:24 +0530

    Update RotateCommand.java

--- project/RotateCommand.java
+++ project/RotateCommand.java
@@ -1,12 +1,5 @@
 public class RotateCommand {
-    private Satellite satellite;
-
-    public RotateCommand(Satellite satellite) {
-        this.satellite = satellite;
-    }
-
-    public void execute(String direction) {
+    public void rotate(Satellite satellite, String direction) {
         satellite.setOrientation(direction);
-        System.out.println("Satellite orientation set to " + direction);
     }
 }
