cd9dc19cb1403bc2895305de6d5953017c75ff61
suryarajan114 <surya19062004@gmail.com>
2024-07-31 15:46:41 +0530

    Update CollectDataCommand.java

--- project/CollectDataCommand.java
+++ project/CollectDataCommand.java
@@ -1,13 +1,6 @@
 public class CollectDataCommand {
-    private Satellite satellite;
-    private static final int DATA_INCREMENT = 10;
-
-    public CollectDataCommand(Satellite satellite) {
-        this.satellite = satellite;
-    }
-
-    public void execute() {
-        satellite.incrementDataCollected(DATA_INCREMENT);
-        System.out.println("Data collected: " + satellite.getDataCollected());
+    public void collectData(Satellite satellite) {
+        satellite.collectData();
     }
 }
+
